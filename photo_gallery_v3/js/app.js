27c90a97cef742ba38910ba9ab2e6e58368bfce2
james braun <braun.james.robert@gmail.com>
2018-05-14 20:14:12 -0500

    search

--- photo_gallery_v3/js/app.js
+++ photo_gallery_v3/js/app.js
@@ -6,14 +6,27 @@ searchElement.setAttribute('type', 'text');
 searchElement.setAttribute('id', 'search');
 searchElement.setAttribute('name', 'user_search');
 searchElement.setAttribute('placeholder', 'Search');
+searchElement.setAttribute('onkeypress', 'getInput()');
+searchElement.setAttribute('value', '');
 document.getElementById('search-bar').appendChild(searchElement);
-
+//document.getElementById('search').onkeypress = function() {getInput()};
 lightbox.option({
     'alwaysShowNavOnTouchDevices': true
 })
 
+function getInputs() {
+    return 'mountain';
+}
+
+
+let inputString = getInputs().toUpperCase();
 $('a').each(function() {
-    let varible = $(this).attr('data-title');
-    varible = varible.toUpperCase();
-    $(this).css('display', 'inline');
+    let varible = $(this).attr('data-title').toUpperCase();
+    if (varible.includes(inputString) === true) {
+        $(this).css('display', 'inline');
+    } else {
+        $(this).css('display', 'none');
+    }
 });
+
+
