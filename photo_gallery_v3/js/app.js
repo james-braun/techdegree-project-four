62232e3e64f1a29d27d37a2222edf2ac5b40ed8f
james braun <braun.james.robert@gmail.com>
2018-05-15 18:19:31 -0500

    working copy before refactoring

--- photo_gallery_v3/js/app.js
+++ photo_gallery_v3/js/app.js
@@ -8,27 +8,25 @@ searchElement.setAttribute('placeholder', 'Search');
 searchElement.setAttribute('value', '');
 document.getElementById('search-bar').appendChild(searchElement);
 
-lightbox.option({
-     'alwaysShowNavOnTouchDevices': true
-});
+// lightbox.option({
+//      'alwaysShowNavOnTouchDevices': true
+// });
 
 $(document).ready(function() {
     let text = '';
     $('input').keydown(function ( event ) {
         let keyPressed = String.fromCharCode(event.which);
-        if (event.which === 8) {
+        if (event.which == 8) {
             text = text.slice(0, -1);
         } else {
             text = text + keyPressed.toUpperCase();
         };
-    });
+    
         
         $('a').each(function() {
             let varible = $(this).attr('data-title');
             if (varible) {
-                //console.log('varible ' + varible);
                 varible = varible.toUpperCase();
-                //console.log('varible ' + varible);
                 if (varible.includes(text) === true) {
                     $(this).css('display', 'inline');
                 } else {
@@ -36,6 +34,6 @@ $(document).ready(function() {
                 };
             };
         });
+    });
 });
-
 
