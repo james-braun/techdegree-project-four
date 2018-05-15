12b0a269641ec3353051c948adca1a3714e16cb2
james braun <braun.james.robert@gmail.com>
2018-05-15 14:45:07 -0500

    mant changes

--- photo_gallery_v3/js/app.js
+++ photo_gallery_v3/js/app.js
@@ -6,27 +6,37 @@ searchElement.setAttribute('type', 'text');
 searchElement.setAttribute('id', 'search');
 searchElement.setAttribute('name', 'user_search');
 searchElement.setAttribute('placeholder', 'Search');
-searchElement.setAttribute('onkeypress', 'getInput()');
+searchElement.setAttribute('onkeypress', 'function(){ return this.value }');
 searchElement.setAttribute('value', '');
 document.getElementById('search-bar').appendChild(searchElement);
 
-lightbox.option({
-    'alwaysShowNavOnTouchDevices': true
-})
-
-function getInputs() {
-    return 'mountain';
-}
-
-
-let inputString = getInputs().toUpperCase();
-$('a').each(function() {
-    let varible = $(this).attr('data-title').toUpperCase();
-    if (varible.includes(inputString) === true) {
-        $(this).css('display', 'inline');
-    } else {
-        $(this).css('display', 'none');
-    }
+$(document).ready(function() {
+    $('input').keypress(function(e) {
+        let keyPessed = $(e).char;
+        console.log(keyPressed);
+        let text = document.getElementById('search').value;
+        text = text.toUpperCase();
+        // if (text.endsWith(`\b`)) {
+        //     text.pop()
+        // }
+        
+        $('a').each(function() {
+            let varible = $(this).attr('data-title');
+            if (varible) {
+                console.log('varible ' + varible);
+                varible = varible.toUpperCase();
+                console.log('varible ' + varible);
+                if (varible.includes(text) == true) {
+                    $(this).css('display', 'inline');
+                } else {
+                    $(this).css('display', 'none');
+                };
+            };
+        });
+    });
 });
 
 
+// lightbox.option({
+//     'alwaysShowNavOnTouchDevices': true
+// });
