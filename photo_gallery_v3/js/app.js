1f80f04c5f74af98b5a52a63d1ab5a63da8f8d53
james braun <braun.james.robert@gmail.com>
2018-05-16 03:54:31 -0500

    many changes

--- photo_gallery_v3/js/app.js
+++ photo_gallery_v3/js/app.js
@@ -14,32 +14,32 @@ lightbox.option({
 
 $(document).ready(function() {
     let search_text = '';
-    $('input').keydown(function ( event ) {
+    $('input').bind("keypress keydown", function ( event ) {
         // get a single character from the keyboard/
         let key_pressed = String.fromCharCode(event.which);
 
         // if key pressed is backspace remove a 
         // letter from the search string.
         // else add it to the string.
-        if (event.which == 8) {
-            search_text = search_text.slice(0, -1);
+        if (event.type == 'keydown') {
+            if (event.keyCode == 8) {
+                search_text = search_text.slice(0, -1);
+            } 
         }
 
-        // if key pressed isn't one of the possible
-        // characters in the caption string then
-        // remove it from the search string.
-        console.log(String.key_pressed.match(/[^a-zA-Z,.! ]/));
-        if (key_pressed.match(/[^a-zA-Z\d,.! ]/) === null) {
-            search_text = search_text + key_pressed;
-        } 
-
+        if (event.type == 'keypress') {
+            if (key_pressed.match(/[^A-Z][^,][^.][^!][^ ]/) === null) {
+                search_text = search_text + key_pressed;
+            }
+        }            
         // if key pressed is a number don't capitalize
         // it otherwise do.
-        console.log(key_pressed.match(/[0123456789]/));
-        if (key_pressed.match(/0-9/) === null) {
-            search_text = search_text.toUpperCase();
-        }
-
+        console.log(event.keyCode);
+        console.log(key_pressed.match(/[^A-Z][^,][^.][^!][^ ]/));
+        // if (key_pressed.match(/[^0-9]/) === null) {
+        //     search_text = search_text.toUpperCase();
+        // }
+        console.log(search_text);
         // get capyion strings from anchor element
         // and test against search text and turn
         // image display on and off accordingly.
