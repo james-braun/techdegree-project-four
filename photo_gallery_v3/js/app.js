c6060b6175bc2070ec37908bc9546667daa8922c
james braun <braun.james.robert@gmail.com>
2018-05-17 02:34:20 -0500

    absolutelly got to be the finsl version

--- photo_gallery_v3/js/app.js
+++ photo_gallery_v3/js/app.js
@@ -16,44 +16,57 @@ lightbox.option({
 
 $(document).ready(function() {
     let search_text = '';
-    let index = -1;
-
-    $('input').bind("keypress keydown", function ( event ) {
+    let index = 0;
+    $('input').bind("keypress keydown click", function ( event ) {
         
         let key_pressed = String.fromCharCode(event.which);
 
         if (event.type == 'keydown') {
             if (event.keyCode == 8) {
-                search_text = search_text.slice(0, index - 1) + search_text.slice(index + 1);
-                index -= 1;
+                if ((search_text.length > index) && (index > 0)) {
+                    search_text = search_text.substring(0, index) + search_text.substring(index + 1);
+                    index -= 1;
+                } else
+                if ((search_text.length == index) && (index > 0)) {
+                    index -= 1;
+                    search_text = search_text.slice(0, index);
+                }
             } else
             if (event.keyCode == 13) {
                 event.preventDefault();
                 key_pressed = null;
             } else 
             if ((event.keyCode == 36) || (event.keyCode == 38)) {
-                index = 0;
+                index = 1;
             } else
-            if (event.keyCode == 37) {
+            if ((event.keyCode == 37) && (index > 0)) {
                 index -= 1;
             } else
             if ((event.keyCode == 35) || (event.keyCode == 40)) {
                 index = search_text.length;
             } else 
-            if ((event.keyCode == 39) && (index < search_text.lenght)){
+            if ((event.keyCode == 39) && (index < search_text.length)) {
                 index += 1
+            } else
+            if (event.keyCode == 46) {
+                if (index < search_text.length) {
+                    search_text = search_text.substring(0, index) + search_text.substring(index + 1);
+                }
             }
         }
-
+        
         if (event.type == 'keypress') {
-            index += 1;
-            console.log(search_text);
             search_text = search_text.slice(0, index) + key_pressed + search_text.slice(index);           
-            console.log(search_text);
-        }      
+            index += 1;
+        }  
+                
+        if (event.type == 'click'){
+            let val = document.getElementById('search').value;
+            index = val.slice(0, document.getElementById('search').selectionStart).length;
+        }
        
         search_text = search_text.toUpperCase();
-        
+        console.log('index ' + index + ' search text ' + search_text + ' length ' + search_text.length);
         $('a').each(function() {
             let caption_string = $(this).attr('data-title');
             if (caption_string) {
